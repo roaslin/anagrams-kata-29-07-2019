29bdcb0d01d91da6bb1f573d9de24ec3ec216534
roaslin <raul.mordillo.sge@mango.com>
2019-07-30 15:36:00 +0200

    refactor literal words

--- src/main/java/AnagramGenerator.java
+++ src/main/java/AnagramGenerator.java
@@ -1,16 +1,20 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class AnagramGenerator {
     public List<String> generateFor(String word) {
 
         if (word.isEmpty()) {
-            return Collections.singletonList("");
+            return new ArrayList<String>() {{
+                add("");
+
+            }};
 
         }
         if (word.length() == 1) {
-            return Collections.singletonList(word);
+            return new ArrayList<String>() {{
+                add(word);
+            }};
         }
 
         if (word.length() == 2) {
@@ -22,14 +26,29 @@ public class AnagramGenerator {
         }
 
         return new ArrayList<String>() {{
-            add(word.substring(0, 1) + generateFor("BC").get(0));
-            add(word.substring(0, 1) + generateFor("BC").get(1));
-            add(word.substring(1, 2) + generateFor("AC").get(0));
-            add(word.substring(1, 2) + generateFor("AC").get(1));
-            add(word.substring(2, 3) + generateFor("AB").get(0));
-            add(word.substring(2, 3) + generateFor("AB").get(1));
+
+            add(word.substring(0, 1) + generateFor(dropCharacterOf(word, 0)).get(0));
+            add(word.substring(0, 1) + generateFor(dropCharacterOf(word, 0)).get(1));
+            add(word.substring(1, 2) + generateFor(dropCharacterOf(word, 1)).get(0));
+            add(word.substring(1, 2) + generateFor(dropCharacterOf(word, 1)).get(1));
+            add(word.substring(2, 3) + generateFor(dropCharacterOf(word, 2)).get(0));
+            add(word.substring(2, 3) + generateFor(dropCharacterOf(word, 2)).get(1));
 
         }};
 
+    }
+
+    private String dropCharacterOf(String word, int position) {
+
+        if (position == 0) {
+            return word.substring(1, 3);
+        }
+
+        if (position == 1) {
+
+            return word.substring(0, 1) + word.substring(2, 3);
+        }
+
+        return word.substring(0, 2);
     }
 }
